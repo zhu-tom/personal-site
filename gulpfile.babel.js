50152f7c03ecd8b2161aa052b53bfe39b2817490
zhu-tom <zhutom01@gmail.com>
2020-12-21 17:37:17 -0500

    add gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,77 @@
+const autoprefixer = require("autoprefixer");
+const browserSync = require("browser-sync");
+const spawn = require("cross-spawn");
+const cssnano = require("cssnano");
+const { dest, series, src, task, watch } = require("gulp");
+const postcss = require("gulp-postcss");
+const atimport = require("postcss-import");
+const tailwindcss = require("tailwindcss");
+
+const SITE_ROOT = "./_site";
+const POST_BUILD_STYLESHEET = `${SITE_ROOT}/assets/css/`;
+const PRE_BUILD_STYLESHEET = "./assets/css/main.css";
+const TAILWIND_CONFIG = "./tailwind.config.js";
+
+// Fix for Windows compatibility
+const jekyll = process.platform === "win32" ? "jekyll.bat" : "jekyll";
+
+const isDevelopmentBuild = process.env.NODE_ENV === "development";
+
+task("buildJekyll", () => {
+  browserSync.notify("Building Jekyll site...");
+
+  const args = ["exec", jekyll, "build"];
+
+  if (isDevelopmentBuild) {
+    args.push("--incremental");
+  }
+
+  return spawn("bundle", args, { stdio: "inherit" });
+});
+
+task("processStyles", () => {
+  browserSync.notify("Compiling styles...");
+
+  return src(PRE_BUILD_STYLESHEET)
+    .pipe(
+      postcss([
+        atimport(),
+        tailwindcss(TAILWIND_CONFIG),
+        ...(isDevelopmentBuild ? [] : [autoprefixer(), cssnano()]),
+      ])
+    )
+    .pipe(dest(POST_BUILD_STYLESHEET));
+});
+
+task("startServer", () => {
+  browserSync.init({
+    files: [SITE_ROOT + "/**"],
+    open: "local",
+    port: 4000,
+    server: {
+      baseDir: SITE_ROOT,
+      serveStaticOptions: {
+        extensions: ["html", "css", "js"],
+      },
+    },
+  });
+
+  watch(
+    [
+      "**/*.css",
+      "**/*.html",
+      "**/*.js",
+      "**/*.md",
+      "**/*.markdown",
+      "!_site/**/*",
+      "!node_modules/**/*",
+    ],
+    { interval: 500 },
+    buildSite
+  );
+});
+
+const buildSite = series("buildJekyll", "processStyles");
+
+exports.serve = series(buildSite, "startServer");
+exports.default = series(buildSite);
